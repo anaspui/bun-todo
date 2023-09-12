e5f741252d29b3f4dc31d3df2170512ef2e40b31
anaspui <omar24md@gmail.com>
2023-09-12 23:34:56 +0600

    Bun is Baking Todos

--- index.tsx
+++ index.tsx
@@ -1,15 +1,42 @@
-console.log("Hello via Bun!");
-Bun.serve({
+import { renderToString } from "react-dom/server"
+
+const server = Bun.serve({
     hostname: "localhost",
     port: 3000,
     fetch: fetchHandler
 })
+console.log(`Bun is baking on ${server.hostname}:${server.port}`)
+
+
 type Todo = { id: number, name: string}
 const todos: Todo[] = []
-function fetchHandler(request: Request): Response{
+
+
+async function fetchHandler(request: Request): Promise<Response>{
     const url = new URL(request.url)
     if (url.pathname === "" || url.pathname === "/") {
         return new Response(Bun.file("index.html"));
     }
+
+    if (url.pathname == "/todos" && request.method == "GET") {
+        return new Response(renderToString(<TodoList todos={todos}></TodoList>))
+    }
+
+    if (url.pathname == "/todos" && request.method == "POST") {
+        const { todo } = await request.json()
+        todos.push({
+            id: todos.length + 1,
+            name: todo,
+        })
+        return new Response(renderToString(<TodoList todos={todos}></TodoList>))
+    }
+    
     return new Response("404 not found", {status: 404})
+}
+
+
+function TodoList(props: { todos: Todo[] }) {
+    return <ul>
+        {props.todos.length ? props.todos.map(todo => <li key={`todo-${todo.id}`}>{todo.name}</li>) : "No TODO found"}
+    </ul>
 }
