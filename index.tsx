144c029c5c5bcf73e97df951d7b71be3c304443a
anaspui <omar24md@gmail.com>
2023-09-12 23:04:50 +0600

    Document & Routing

--- index.tsx
+++ index.tsx
@@ -1,1 +1,15 @@
 console.log("Hello via Bun!");
+Bun.serve({
+    hostname: "localhost",
+    port: 3000,
+    fetch: fetchHandler
+})
+type Todo = { id: number, name: string}
+const todos: Todo[] = []
+function fetchHandler(request: Request): Response{
+    const url = new URL(request.url)
+    if (url.pathname === "" || url.pathname === "/") {
+        return new Response(Bun.file("index.html"));
+    }
+    return new Response("404 not found", {status: 404})
+}
